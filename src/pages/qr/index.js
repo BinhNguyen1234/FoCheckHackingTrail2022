eb20bb17469df552174b17b11a0b4dfea6696e26
TanNguyenVN <tan.nguyen@getcerebral.com>
2022-07-10 05:44:30 +0700

    update

--- src/pages/qr/index.js
+++ src/pages/qr/index.js
@@ -1,16 +1,34 @@
+import {
+  navigateToPDP,
+} from "../../utils/navigate";
+
 Page({
   onScan(e) {
     my.scan({
       success: (res) => {
         const content = res.toString()
-        console.log(res,content)
         my.alert({
           title: 'Found',
-          content: res
+          content: content,
+          buttonText: 'Alert Button',
+          success: () => {
+            console.log('Success');
+            navigateToPDP(content)
+          },
+          fail: () => {
+            console.log('Fail');
+          },
+          complete: () => {
+            console.log('Complete');
+          }
         })
       },
       fail: (e) => {
         console.log(e);
+        my.alert({
+          title: 'Lỗi rồi sếp ơi',
+          content: e
+        })
       }
     });
   }
